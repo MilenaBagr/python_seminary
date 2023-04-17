f9176532fa6463d9449b9c1d02d0b9e089f139d9
Milena <bagramanmilena@gmail.com>
2023-04-17 17:45:13 +0300

    программа, высчитывающая количество ясных дней подряд для синоптиков

---
+++ Task13/13.py
@@ -0,0 +1,17 @@
+import random
+
+length = 30
+count = 0
+max_count = 0
+day = 0
+
+for i in range(length):
+    day += random.randint(-3,3)
+    print(day, end = ' ')
+    if day > 0:
+        count += 1
+        if count > max_count:
+            max_count = count
+    else:
+        count = 0
+print(f'\nМаксимальное число теплых дней подряд {max_count}.')
